03340456d5eb6fbc4ded72d95d765610ad614905
Evan Kim <ekim2339@gmail.com>
2024-06-30 09:52:43 -0400

    next framework outline

---
+++ app/posts/loading.js
@@ -0,0 +1,7 @@
+/* This file is used to create a loading component for posts pages */
+
+const Loading = () => {
+  return <LoadingSkeleton />;
+};
+
+export default Loading;
---
+++ app/posts/new/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// /posts/new
+
+const page = () => {
+  return (
+    <div>
+      <h1>NEW POST</h1>
+    </div>
+  );
+};
+
+export default page;
